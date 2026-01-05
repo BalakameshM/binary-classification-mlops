7e69da5784a1eacdc61e6238124035287764b844
Bala Kamesh M <balamariappan20@gmail.com>
2026-01-06 01:56:18 +0530

    Remove editor history from repo

--- .history/src/evaluation/plots_20260105232141.py
+++
@@ -1,9 +0,0 @@
-# src/evaluation/plots.py
-import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay
-
-def plot_confusion_matrix(y_true, y_pred, title="Confusion Matrix"):
-    ConfusionMatrixDisplay.from_predictions(y_true, y_pred)
-    plt.title(title)
-    plt.tight_layout()
-    return plt.gcf()
--- .history/tests/unit/test_load_local_20260105232256.py
+++
@@ -1,5 +0,0 @@
-from src.data.load_local import load_cereal_csv
-
-def test_load_local(raw_csv_path):
-    df = load_cereal_csv(raw_csv_path)
-    assert df.shape[0] > 0
